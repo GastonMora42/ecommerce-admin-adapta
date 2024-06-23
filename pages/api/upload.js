40b353eadd7fc4cd55c0f793d71a8fef4f520b43
GastonMora42 <gastonmora1742@gmail.com>
2024-06-23 20:51:54 -0300

    Podemos cargar productos con la FOTO correctamente y se almacenan en google storage en un bucket

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,47 +1,50 @@
-import multiparty from 'multiparty';
-import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import { Storage } from '@google-cloud/storage';
+import path from 'path';
 import fs from 'fs';
 import mime from 'mime-types';
-import {mongooseConnect} from "@/lib/mongoose";
-import {isAdminRequest} from "@/pages/api/auth/[...nextauth]";
-const bucketName = 'dawid-next-ecommerce';
+import multiparty from 'multiparty';
+import { mongooseConnect } from "@/lib/mongoose";
+import { isAdminRequest } from "@/pages/api/auth/[...nextauth]";
 
-export default async function handle(req,res) {
+const keyFilename = path.join(process.cwd(), 'keyfile.json');
+const bucketName = 'bucket-adapta';
+
+export default async function handle(req, res) {
   await mongooseConnect();
-  await isAdminRequest(req,res);
+  await isAdminRequest(req, res);
 
+  const storage = new Storage({ keyFilename });
   const form = new multiparty.Form();
-  const {fields,files} = await new Promise((resolve,reject) => {
+  const { fields, files } = await new Promise((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
       if (err) reject(err);
-      resolve({fields,files});
+      resolve({ fields, files });
     });
   });
-  console.log('length:', files.file.length);
-  const client = new S3Client({
-    region: 'us-east-1',
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    },
-  });
+
   const links = [];
   for (const file of files.file) {
     const ext = file.originalFilename.split('.').pop();
     const newFilename = Date.now() + '.' + ext;
-    await client.send(new PutObjectCommand({
-      Bucket: bucketName,
-      Key: newFilename,
-      Body: fs.readFileSync(file.path),
-      ACL: 'public-read',
-      ContentType: mime.lookup(file.path),
-    }));
-    const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
-    links.push(link);
+    await storage.bucket(bucketName).upload(file.path, {
+      destination: newFilename,
+      metadata: {
+        contentType: mime.lookup(file.path),
+        cacheControl: 'public, max-age=31536000',
+      },
+    });
+
+    const [url] = await storage.bucket(bucketName).file(newFilename).getSignedUrl({
+      action: 'read',
+      expires: '03-09-2491', // Puedes ajustar esta fecha según sea necesario
+    });
+
+    links.push(url);
   }
-  return res.json({links});
+
+  return res.json({ links });
 }
 
 export const config = {
-  api: {bodyParser: false},
+  api: { bodyParser: false },
 };
